dfb5dc82808a0ae45cf586cc0ca1ba57b297e8ce
ngsonha <sonhanbk@gmail.com>
2021-07-14 09:43:53 +0700

    fix with request

---
+++ resources/lang/en/messenger.php
@@ -0,0 +1,12 @@
+<?php 
+    return [
+        'email'=>'Email Address',
+        'logout'=>'logout',
+        'password' => 'The password is incorrect',
+        'user'=>'User has been use',
+        'input_user'=>'input user',
+        
+    ];
+
+
+?>
---
+++ resources/lang/vi/messenger.php
@@ -0,0 +1,11 @@
+<?php 
+    return [
+        'email'=>'Địa chỉ email',
+        'logout'=>'Đăng xuất',
+        'password' => 'Mật khẩu không đúng.',
+        'user'=>'Người dùng đã tồn tại',
+        'input_user'=>'Nhập tên tài khoản',
+    ];
+
+
+?>
